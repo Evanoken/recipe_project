12f3f16904ca273e08f10c2613bd69ef67734269
Evecandy <evecandymutunga@gmail.com>
2023-05-23 15:47:51 +0300

    Added an image food.jpg

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import Header from './components/Header'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import About from './pages/About'
-import About from './components/About'
+
 import Home from './pages/Home'
 import Favourites from './pages/Favourites'
 // import Recipe from './pages/Recipe'
 import NotFound from './pages/NotFound'
 
-import './App.css'
+
 
 function App() {
   return (
