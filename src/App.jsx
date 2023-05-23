9333fc014f8f2426b82bb21ed1a2a0869368bacc
freshia-njoki <fnjokimacharia@gmail.com>
2023-05-23 15:25:10 +0300

    modified app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import Header from './components/Header'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './pages/About'
+import About from './components/About'
+import Home from './pages/Home'
+import Favourites from './pages/Favourites'
+// import Recipe from './pages/Recipe'
+import NotFound from './pages/NotFound'
+
+import './App.css'
 
 function App() {
-  
-
   return (
     <>
-      <Header/>
-        
+    <BrowserRouter>
+      <Header />
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/recipe" element={<Recipe />}/>
+      <Route path="/favourites" element={<Favourites />}/>
+      <Route path="/about" element={ <About /> }/>
+      <Route path="*" element={<NotFound />}/>
+    </Routes>
+    <Footer />
+    </BrowserRouter>
+    <About />
+    <NotFound />
+    <Favourites />
     </>
   )
 }
